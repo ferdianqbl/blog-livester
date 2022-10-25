d9c41efc6ad51d12afea7f495b65c2043ffea82e
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-10-25 09:15:41 +0700

    create homepage

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -94,5 +94,131 @@
     </div>
 
   </section>
+
+  <section class="author-choice" id="author-choice">
+    <div class="container">
+      <div class="row justify-content-center align-items-center">
+        <div class="home-header-section-group d-flex justify-content-between align-items-center">
+          <h2 class="home-section-title">Author's Choice</h2>
+          <a href="/" class="btn-custom-primary">See All <i class="bi bi-arrow-right"></i></a>
+        </div>
+        <div class="col-md-3">
+          <div class="card-post">
+            <img src="/images/home/sub-content (3).png" alt="card-post" class="card-post-img img-fluid">
+
+            <div class="card-post-body">
+              <p class="card-post-author">Alexander | March 12, 2021</p>
+              <a href="/" class="nav-link card-post-category ai-color">AI</a>
+              <p class="card-post-title">Seeking Experience in the Field of Data Visualization Experts</p>
+              <p class="card-post-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <a href="#" class="nav-link card-post-link">Read More <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card-post">
+            <img src="/images/home/sub-content (3).png" alt="card-post" class="card-post-img img-fluid">
+
+            <div class="card-post-body">
+              <p class="card-post-author">Alexander | March 12, 2021</p>
+              <a href="/" class="nav-link card-post-category ai-color">AI</a>
+              <p class="card-post-title">Seeking Experience in the Field of Data Visualization Experts</p>
+              <p class="card-post-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <a href="#" class="nav-link card-post-link">Read More <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card-post">
+            <img src="/images/home/sub-content (3).png" alt="card-post" class="card-post-img img-fluid">
+
+            <div class="card-post-body">
+              <p class="card-post-author">Alexander | March 12, 2021</p>
+              <a href="/" class="nav-link card-post-category ai-color">AI</a>
+              <p class="card-post-title">Seeking Experience in the Field of Data Visualization Experts</p>
+              <p class="card-post-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <a href="#" class="nav-link card-post-link">Read More <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card-post">
+            <img src="/images/home/sub-content (3).png" alt="card-post" class="card-post-img img-fluid">
+
+            <div class="card-post-body">
+              <p class="card-post-author">Alexander | March 12, 2021</p>
+              <a href="/" class="nav-link card-post-category ai-color">AI</a>
+              <p class="card-post-title">Seeking Experience in the Field of Data Visualization Experts</p>
+              <p class="card-post-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <a href="#" class="nav-link card-post-link">Read More <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="home-category" id="home-category">
+    <div class="container">
+      <div class="row justify-content-center align-items-center">
+        <div class="home-header-section-group d-flex justify-content-between align-items-center">
+          <h2 class="home-section-title">By Categories</h2>
+          <a href="/" class="btn-custom-primary">See All <i class="bi bi-arrow-right"></i></a>
+        </div>
+        <div class="col-md-3">
+          <div class="card-post">
+            <img src="/images/home/sub-content (3).png" alt="card-post" class="card-post-img img-fluid">
+
+            <div class="card-post-body">
+              <p class="card-post-author">Alexander | March 12, 2021</p>
+              <a href="/" class="nav-link card-post-category ai-color">AI</a>
+              <p class="card-post-title">Seeking Experience in the Field of Data Visualization Experts</p>
+              <p class="card-post-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <a href="#" class="nav-link card-post-link">Read More <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card-post">
+            <img src="/images/home/sub-content (3).png" alt="card-post" class="card-post-img img-fluid">
+
+            <div class="card-post-body">
+              <p class="card-post-author">Alexander | March 12, 2021</p>
+              <a href="/" class="nav-link card-post-category web-color">Web</a>
+              <p class="card-post-title">Seeking Experience in the Field of Data Visualization Experts</p>
+              <p class="card-post-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <a href="#" class="nav-link card-post-link">Read More <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card-post">
+            <img src="/images/home/sub-content (3).png" alt="card-post" class="card-post-img img-fluid">
+
+            <div class="card-post-body">
+              <p class="card-post-author">Alexander | March 12, 2021</p>
+              <a href="/" class="nav-link card-post-category ai-color">AI</a>
+              <p class="card-post-title">Seeking Experience in the Field of Data Visualization Experts</p>
+              <p class="card-post-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <a href="#" class="nav-link card-post-link">Read More <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card-post">
+            <img src="/images/home/sub-content (3).png" alt="card-post" class="card-post-img img-fluid">
+
+            <div class="card-post-body">
+              <p class="card-post-author">Alexander | March 12, 2021</p>
+              <a href="/" class="nav-link card-post-category design-color">Design</a>
+              <p class="card-post-title">Seeking Experience in the Field of Data Visualization Experts</p>
+              <p class="card-post-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <a href="#" class="nav-link card-post-link">Read More <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </div>
 @endsection
